20a5c05f5e9bb037b34e8ab95c8f6e39ff6d7f93
MD QUAIS <36109352+MdQuais@users.noreply.github.com>
2019-03-24 22:27:11 +0530

    initial commit

---
+++ home.php
@@ -0,0 +1,46 @@
+<?php
+echo "<h1 text align=center>Library Management<h1>";
+?>
+<html>
+    <head>
+        <title>Home</title>
+         <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+    <h2> Hello friends</h2>
+    
+    <div id="nave">
+       <ul>
+           <li><b> <a href="home.php">Home</a></b></li>
+           <li><b> <a href="top.php">Top rated book</a></b></li>
+           <li><b> <a href="contact.php">Contact Us</a></b></li>
+            <li> <b><a href="about.php">About Us</a></b></li>
+           <li> <b><a href="block.php">Block</a></b></li>
+           <li> <b><a href="help.php">Help</a></b></li>
+           
+       </ul>
+    </div>
+    
+    <div id="container">
+          <div id="left"><br><br>
+             <center> <img src="img/lib.jpg" width="500px"></center>
+             <br>
+             <h3 style="color:red;margin-left:50px">Top Readed Books</h3>
+               <div id="a"><img src="img/a.jpg"></div>
+               <div id="b"><img src="img/b.jpg"></div>
+               <div id="c"><img src="img/b.jpg"> </div>
+          </div>
+          <div id="right">
+          <h1 style="color:red">Category</h1>
+           <hr>
+              <a href="#" > <b> Romantic</b></a> <br><br><br><br>
+                <a href="#" > <b>  Biography</b></a> <br><br><br><br>
+                <a href="#" > <b>  Novels</b></a> <br><br><br><br>
+               <a href="#" >  <b> Suspence</b></a> <br><br><br><br>
+           
+            </div>
+     </div>
+    </body>
+   </html>
+
+
